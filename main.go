4dd35aeadc6d1dbd101e506267f1d338c60c36ce
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2017-07-18 12:08:21 -0700

    Print ping statistics at the end

--- main.go
+++ main.go
@@ -42,10 +42,16 @@ func main() {
 		}
 		lastPing = pkt.Seq
 		if pkt.Seq > 0 && pkt.Seq%10 == 0 {
+			fmt.Printf("%d bytes from %s: icmp_seq=%d time=%v\n", pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt)
 			fmt.Printf("lost packets: %v\n", lostPackets)
 		}
 	}
 	pinger.OnFinish = func(stats *ping.Statistics) {
+		fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
+		fmt.Printf("%d packets transmitted, %d packets received, %v%% packet loss\n",
+			stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
+		fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
+			stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
 		fmt.Printf("lost packets: %v\n", lostPackets)
 	}
 
